54d7d6be27f558e97aca60c8aab64187653d137f
Dru <druashley@gmail.com>
2021-10-13 08:21:44 -0700

    spelling corrections found for the homepagesteps. Cleaned up the contact us and footer. Removed the link to the about-us page for now.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,25 +5,29 @@ export default function Footer() {
   return (
     <div className=" min-h-48  mt-24  items-center bg-accent">
       <div className="container mx-auto flex flex-col md:flex-row justify-between text-center py-12 ">
-        <div className="p-2 text-4xl font-medium tracking-tighter text-white">
+        <Link
+          to="/"
+          className="p-2 text-4xl font-medium tracking-tighter text-white"
+          onClick={() => window.scrollTo(0, 0)}
+        >
           OurProtocol
-        </div>
+        </Link>
         <div className="flex flex-col justify-between p-2 text-white text-xl">
           <Link
             to="/contact-us"
             className="cursor-pointer hover:text-gray-300 hover:underline"
             onClick={() => window.scrollTo(0, 0)}
           >
-            Contact
+            Contact Us
           </Link>
 
-          <Link
+          {/* <Link
             to="about-us"
             className="cursor-pointer hover:text-gray-300 hover:underline"
             onClick={() => window.scrollTo(0, 0)}
           >
             About Us
-          </Link>
+          </Link> */}
         </div>
       </div>
     </div>
