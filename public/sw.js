cd9fc7c9c8325796b49d7e5f7ad83cdda603c437
Dru <druashley@gmail.com>
2021-11-04 10:02:53 -0700

    service worker changes.

--- public/sw.js
+++ public/sw.js
@@ -4,19 +4,19 @@ this.addEventListener("install", (e) => {
   // Using caches.delete here to remove the old data. I don't think this would be correct for a production build.
   caches.delete(cacheData);
   const files = [
-    "/static/js/main.chunk.js",
-    "/static/js/bundle.js",
-    "/static/js/vendors~main.chunk.js",
-    "/static/js/vendors~main.chunk.js.map",
-    "/index.html",
-    "/",
-    "/dashboard",
-    "/static/js/main.chunk.js.map",
-    "/logo1.svg",
-    "/offline-dashboard",
-    "/static/media/notes.8c01160c.svg",
-    "/static/js/2.902b1c71.chunk.js",
-    "/static/js/main.b504a0f8.chunk.js",
+    "./static/js/main.chunk.js",
+    "./static/js/bundle.js",
+    "./static/js/vendors~main.chunk.js",
+    "./static/js/vendors~main.chunk.js.map",
+    "./index.html",
+    "./",
+    "./dashboard",
+    "./static/js/main.chunk.js.map",
+    "./logo1.svg",
+    "./offline-dashboard",
+    "./static/media/notes.8c01160c.svg",
+    "./static/js/2.902b1c71.chunk.js",
+    "./static/js/main.b504a0f8.chunk.js",
   ];
 
   e.waitUntil(
